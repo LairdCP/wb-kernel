bd071ee6a3acece4a87cb9f1dfaf3de26cfc7026
Dan Kephart <dan.kephart@lairdtech.com>
2013-08-09 17:57:44 -0400

    Fixing bug where all but one channel was cut off message due to impropery malloc and cpy

--- drivers/net/wireless/ath/ath6kl/wmiconfig.c
+++ drivers/net/wireless/ath/ath6kl/wmiconfig.c
@@ -92,11 +92,14 @@ void ath6kl_wmicfg_send_stats(struct ath6kl_vif *vif,
 void ath6kl_wmicfg_send_channel_list_reply(struct wmi *wmi, 
 										   struct wmi_channel_list_reply *reply)
 {
-	u32 *buff = kzalloc(sizeof(*reply) + 4, GFP_KERNEL);
-
+	size_t reply_len = sizeof(*reply) + (sizeof(__le16) * (reply->num_ch - 1));
+	u32 *buff = kzalloc(reply_len + 4, GFP_KERNEL);
+	
 	buff[0] = WMI_GET_CHANNEL_LIST_CMDID;
-	memcpy(buff+1, reply, sizeof(struct wmi_channel_list_reply));
-	ath6kl_tm_rx_wmi_event(wmi->parent_dev, buff, sizeof(struct wmi_channel_list_reply)+4);
+	memcpy(buff+1, reply, reply_len);
+	
+	ath6kl_tm_rx_wmi_event(wmi->parent_dev, buff, reply_len + 4);
+	
 	kfree(buff);
 }
 
