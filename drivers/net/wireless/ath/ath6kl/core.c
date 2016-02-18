62d12332f7f3aab869e01b4c3ebe8496ce21dafc
Dan Kephart <dan.kephart@lairdtech.com>
2016-02-18 12:15:27 -0500

    laird: ath6kl: Change btcoex params from int to char to fix warning
    For btcoex_chip_type and btcoex_ant_type, both are specified as byte for
    the module parameter but unsigned int as the variable.  Fix variable to be
    unsigned char to match parameter type of byte.
    
    Bug: 9053

--- drivers/net/wireless/ath/ath6kl/core.c
+++ drivers/net/wireless/ath/ath6kl/core.c
@@ -40,8 +40,8 @@ static unsigned int ath6kl_p2p;
 static unsigned int testmode;
 static unsigned int recovery_enable;
 static unsigned int heart_beat_poll;
-static unsigned int btcoex_chip_type;
-static unsigned int btcoex_ant_config;
+static unsigned char btcoex_chip_type;
+static unsigned char btcoex_ant_config;
 
 module_param(debug_mask, uint, 0644);
 module_param(suspend_mode, uint, 0644);
