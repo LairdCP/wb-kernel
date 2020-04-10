8c53440a5f399985c5e726fe578ed81779fe194b
Dean Ramsier <dean.ramsier@lairdconnect.com>
2020-04-10 09:34:56 -0400

    Revert "brcmfmac: calling skb_orphan before sending skb to SDIO bus"
    This reverts commit 284c603d9a479d0bdaf659cdc8a0ed34be7bd85e.

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -2324,7 +2324,6 @@ static uint brcmf_sdio_sendfromq(struct brcmf_sdio *bus, uint maxframes)
 					      &prec_out);
 			if (pkt == NULL)
 				break;
-			skb_orphan(pkt);
 			__skb_queue_tail(&pktq, pkt);
 		}
 		spin_unlock_bh(&bus->txq_lock);
