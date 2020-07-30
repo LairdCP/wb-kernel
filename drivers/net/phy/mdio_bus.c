98b3a89e1bc3a1bcef0f08532a03830ad3714aad
Bruno Thomsen <bruno.thomsen@gmail.com>
2020-07-30 21:57:48 +0200

    net: mdiobus: add reset-post-delay-us handling
    Load new "reset-post-delay-us" value from MDIO properties,
    and if configured to a greater then zero delay do a
    flexible sleeping delay after MDIO bus reset deassert.
    This allows devices to exit reset state before start
    bus communication.
    
    Signed-off-by: Bruno Thomsen <bruno.thomsen@gmail.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/mdio_bus.c
+++ drivers/net/phy/mdio_bus.c
@@ -399,6 +399,8 @@ int __mdiobus_register(struct mii_bus *bus, struct module *owner)
 		gpiod_set_value_cansleep(gpiod, 1);
 		udelay(bus->reset_delay_us);
 		gpiod_set_value_cansleep(gpiod, 0);
+		if (bus->reset_post_delay_us > 0)
+			usleep_range(bus->reset_post_delay_us, bus->reset_post_delay_us + 100);
 	}
 
 	if (bus->reset)
