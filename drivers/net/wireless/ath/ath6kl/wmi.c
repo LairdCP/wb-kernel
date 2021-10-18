6ac9139c05f87bb7f93d629401705da54ff8a029
Service Build Slave <it.devops@lairdconnect.com>
2021-10-18 16:34:07 +0000

    bump to build number 7.0.0.628

--- drivers/net/wireless/ath/ath6kl/wmi.c
+++ drivers/net/wireless/ath/ath6kl/wmi.c
@@ -77,7 +77,7 @@ static const u8 up_to_ac[] = {
 
 /* Laird build number is 32bit value.  Parsed in the form w.x.y.z.
  */
-#define LAIRD_BUILD_NUMBER 0x070000273
+#define LAIRD_BUILD_NUMBER 0x070000274
 
 static void __lrd_set_AP_Name(struct wmi *wmi, const char *apname);
 static void __lrd_set_AP_IP(struct wmi *wmi, const char *apip);
